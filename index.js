99fb2782f8392b580a9fa58dbc0b96206064e459
Yusfiiiii <yusufeymenozcan12@gmail.com>
2025-11-17 15:21:45 +1200

    Fix import statement for keep_alive module

--- index.js
+++ index.js
@@ -72,6 +72,6 @@ client.on("presenceUpdate", async (oldPresence, newPresence) => {
 });
 
 client.login(process.env.TOKEN);
-import keep_alive from './keep_alive.js';
+import keep-alive from './keep_alive.js';
 
 keep_alive();
